a5653ce27ffeab1a5ef7f59fbbb2a1dfc9df1b85
Yovie Alfa Guistuta <yoviealfaguistuta@gmail.com>
2022-07-04 02:34:55 +0700

    Modify landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -30,48 +30,66 @@
     <div class="banner">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://foto.data.kemdikbud.go.id/getImage/10807072/17.jpg"
-                        class="d-block w-100 fixed-size-banner" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Maksimalkan Pengalaman Anda Dengan Mengikuti Kegiatan
-                            Praktik Kerja Lapangan (PKL)</h5>
-                        <p>Dengan mengikuti kegiatan PKL ini, siswa/i diharapkan dapat meningkatkan keterampilan
-                            dan memperluas wawasan, serta dapat menerapkan ilmu-ilmu yang telah dipelajari di sekolah
-                            pada dunia kerja atau dunia usaha.
-                        </p>
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/IMG_0100.JPG') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Maksimalkan Pengalaman Anda Dengan Mengikuti Kegiatan Praktik Kerja Lapangan (PKL)</h1>
+                        <p>Dengan mengikuti kegiatan PKL ini, siswa/i diharapkan dapat meningkatkan keterampilan dan memperluas wawasan, serta dapat menerapkan ilmu-ilmu yang telah dipelajari di sekolah pada dunia kerja atau dunia usaha.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="https://foto.data.kemdikbud.go.id/getImage/10807072/17.jpg"
-                        class="d-block w-100 fixed-size-banner" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Maksimalkan Pengalaman Anda Dengan Mengikuti Kegiatan
-                            Praktik Kerja Lapangan (PKL)</h5>
-                        <p>Dengan mengikuti kegiatan PKL ini, siswa/i diharapkan dapat meningkatkan keterampilan
-                            dan memperluas wawasan, serta dapat menerapkan ilmu-ilmu yang telah dipelajari di sekolah
-                            pada dunia kerja atau dunia usaha.
-                        </p>
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/IMG_8679.JPG') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Jurusan Teknik Instalasi Tenaga Listrik</h1>
+                        <p>Teknik Instalasi Tenaga Listrik adalah jurusan yang mempelajari tentang perencanaan dan pemasangan instalasi penerangan, tenaga pemasangan dan pengoperasian motor listrik  dengan kendali elektromekanik, elektronik dan PLC (Programable Logic Controller). Merawat dan memperbaiki alat rumah tangga listrik dan teknik pendingin, serta menggulung ulang motor listrik.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="https://foto.data.kemdikbud.go.id/getImage/10807072/17.jpg"
-                        class="d-block w-100 fixed-size-banner" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Maksimalkan Pengalaman Anda Dengan Mengikuti Kegiatan
-                            Praktik Kerja Lapangan (PKL)</h5>
-                        <p>Dengan mengikuti kegiatan PKL ini, siswa/i diharapkan dapat meningkatkan keterampilan
-                            dan memperluas wawasan, serta dapat menerapkan ilmu-ilmu yang telah dipelajari di sekolah
-                            pada dunia kerja atau dunia usaha.
-                        </p>
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/TP.jpg') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Jurusan Teknik Pemesinan</h1>
+                        <p>Teknik Pemesinan adalah jurusan yang mempelajari tentang energi dan sumber energinya. Hal-hal yang dipelajari dalam teknik mesin banyak berurusan dengan penggerak-penggerak awal seperti turbin uap, motor bakar, mesin-mesin perkakas, pompa dan kompresor, pendingin dan pemanas dan alat-alat kimia tertentu.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/TKJ.jpg') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Jurusan Teknik Komputer dan Jaringan</h1>
+                        <p>Teknik Komputer dan Jaringan merupakan ilmu berbasis Teknologi Informasi dan Komunikasi terkait kemampuan algoritma, dan pemrograman komputer, perakitan komputer, perakitan jaringan komputer, dan pengoperasian perangkat lunak, dan internet.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/IMG_8690.JPG') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Jurusan Teknik Audio Video</h1>
+                        <p>Teknik Audio Video adalah kompetensi keahlian (atau umum disebut jurusan) yang akan membekali siswa dengan pengetahuan, sikap dan ketrampilan di bidang elektronika, khususnya dalam pengolahan sistem audio dan video.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="ctx"></div>
+                    <img src="{{ url('assets/images/slider/TSM.jpg') }}" class="d-block w-100 fixed-size-banner image-overlay img-ctx" alt="...">
+                    <div class="carousel-caption d-none d-md-block ctm" style="bottom: 29.25rem">
+                        <img class="logo-banner" src="{{ url('logo.png') }}" />
+                        <h1 class="title-banner">Jurusan Teknik Sepeda Motor</h1>
+                        <p>Teknik dan Bisnis Sepeda Motor (TBSM) adalah salah satu cabang ilmu teknik mesin yang mempelajari tentang bagaimana merancang, membuat dan mengembangkan alat-alat transportasi darat yang menggunakan mesin, terutama sepeda motor. Teknik dan Bisnis Sepeda Motor (TBSM) menggabungkan elemen-elemen pengetahuan mekanika, listrik, elektronik, keselamatan dan lingkungan serta matematika, fisika, kimia, ipa dan manajemen.</p>
                     </div>
                 </div>
             </div>
@@ -94,27 +112,27 @@
         <div class="tools">
             <div class="container-side-d">
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/1.png') }}" class="icon-image-landing mb-3"/>
                     <p>Membentuk pola pikir yang konstruktif</p>
                 </div>
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/2.png') }}" class="icon-image-landing mb-3"/>
                     <p>Melatih siswa untuk berkomunikasi secara profesional </p>
                 </div>
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/3.png') }}" class="icon-image-landing mb-3"/>
                     <p>Menambah  keterampilan, dan pengalaman siswa </p>
                 </div>
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/4.png') }}" class="icon-image-landing mb-3"/>
                     <p>Menjalin kerja sama yang baik antara sekolah dan perusahaan terkait </p>
                 </div>
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/5.png') }}" class="icon-image-landing mb-3"/>
                     <p>Mengenalkan siswa pada pekerjaan lapangan yang sebenarnya </p>
                 </div>
                 <div class="side-d">
-                    <i class="fa-solid fa-head-side-medical"></i>
+                    <img src="{{ url('assets/images/icon/6.png') }}" class="icon-image-landing mb-3"/>
                     <p>Mempersiapkan sumber daya manusia berkualitas </p>
                 </div>
             </div>

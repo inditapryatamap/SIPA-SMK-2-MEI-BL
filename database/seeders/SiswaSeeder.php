96cf6ec2bf12d423a2660b528ca3d89dfe1a47b6
Yovie Alfa Guistuta <yoviealfaguistuta@gmail.com>
2022-07-11 09:43:15 +0700

    Adding siswa seeder

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -19,8 +19,22 @@ class SiswaSeeder extends Seeder
         $siswa = new Siswa();
         $siswa->id = 1;
         $siswa->email = 'siswa@gmail.com';
-        $siswa->nama = 'Yovie Alfa Guistuta';
+        $siswa->nama = 'Rendi Alvianida';
         $siswa->nis = '123456789';
+        $siswa->id_jurusan = 1;
+        $siswa->jenis_kelamin = 'Laki Laki';
+        $siswa->ttl = 'Bandar Lampung, 5 Maret 1999';
+        $siswa->no_telpon = '+62895636134374';
+        $siswa->password = bcrypt('12345678');
+        $siswa->created_at = Carbon::now();
+        $siswa->updated_at = Carbon::now();
+        $siswa->save();
+
+        $siswa = new Siswa();
+        $siswa->id = 2;
+        $siswa->email = 'komang@gmail.com';
+        $siswa->nama = 'Komang Kresna P';
+        $siswa->nis = '123451234';
         $siswa->id_jurusan = 1;
         $siswa->jenis_kelamin = 'Laki Laki';
         $siswa->ttl = 'Bandar Lampung, 5 Maret 1999';
